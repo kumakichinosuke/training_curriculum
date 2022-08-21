0ec38fa5f5f38bb8215b8c826b3f519cf69296e1
kumakichinosuke <kumakichi770@gmail.com>
2022-08-21 14:52:35 +0900

    issue４−６の解消

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -8,6 +8,7 @@ class CalendarsController < ApplicationController
 
   # 予定の保存
   def create
+  
     Plan.create(plan_params)
     redirect_to action: :index
   end
@@ -15,7 +16,7 @@ class CalendarsController < ApplicationController
   private
 
   def plan_params
-    params.require(:calendars).permit(:date, :content)
+    params.require(:plan).permit(:date, :plan)
   end
 
   def get_Week
@@ -35,8 +36,8 @@ class CalendarsController < ApplicationController
         today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
 
-      wwday_num = # wdayメソッドを用いて取得した数値
-      if #「wday_numが7以上の場合」という条件式
+      wday_num = Date.today.wday
+      if wday_num > 7
         wday_num = wday_num -7
       end
 
